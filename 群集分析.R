49eeea1466ef832591cecc5ec2156b129764e237
myhuang17 <98328497+myhuang17@users.noreply.github.com>
2022-05-13 16:51:38 +0800

    Update 群集分析.R

--- 群集分析.R
+++ 群集分析.R
@@ -1,3 +1,4 @@
+library(corrplot)#相關係數圖
 #資料讀取與初步剖析
 data = read.csv("C:\\Users\\user\\Desktop\\多變量報告\\data\\winequality-red.csv")
 sum(is.na(data))#遺失值
@@ -16,11 +17,23 @@ plot(h.E.cluster, xlab="歐式距離")
 h.M.cluster <- hclust(M.dist) 
 plot(h.M.cluster, xlab="曼哈頓距離")
 #可以在hclust()裡面調整參數method，選擇不同的方法：
-hclust(E.dist, method="single")   #歐-最近法
-hclust(E.dist, method="complete") #歐-最遠法
-hclust(E.dist, method="average")  #歐-平均法
-hclust(E.dist, method="centroid") #歐-中心法
-hclust(E.dist, method="ward.D2")  #歐-華德法
+en = hclust(E.dist, method="single")   #歐-最近法
+ef = hclust(E.dist, method="complete") #歐-最遠法
+ea = hclust(E.dist, method="average")  #歐-平均法
+ec = hclust(E.dist, method="centroid") #歐-中心法
+ew = hclust(E.dist, method="ward.D2")  #歐-華德法
 
-plot(hclust(E.dist, method="single"))
-abline(h=100, col="red")
+mn = hclust(M.dist, method="single")   #曼-最近法
+mf = hclust(M.dist, method="complete") #曼-最遠法
+ma = hclust(M.dist, method="average")  #曼-平均法
+mc = hclust(M.dist, method="centroid") #曼-中心法
+mw = hclust(M.dist, method="ward.D2")  #曼-華德法
+
+resulten = cutree(en, k=6)
+table(resulten, data$quality)
+
+resultmn = cutree(mn, k=6)
+table(resultmn, data$quality)
+
+plot(ef)
+abline(h=18, col="red")
