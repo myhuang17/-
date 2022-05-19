acf6f73b04d05573e342ffaf545b0397a896749e
myhuang17 <98328497+myhuang17@users.noreply.github.com>
2022-05-19 22:51:42 +0800

    Update 主成分分析.R

--- 主成分分析.R
+++ 主成分分析.R
@@ -1,7 +1,3 @@
-library(corrplot)
-library(ggplot2)
-library(dplyr)
-library(GGally)
 #讀取資料
 data = read.csv("C:\\Users\\user\\Desktop\\多變量報告\\data\\winequality-red.csv")
 data = data[,-12]
@@ -12,26 +8,29 @@ pca = prcomp(formula = ~ fixed.acidity+volatile.acidity+citric.acid
              +residual.sugar+chlorides+free.sulfur.dioxide
              +total.sulfur.dioxide+density
              +pH+sulphates+alcohol, data = data, scale = TRUE)
+write.csv(pca$x,"C:\\Users\\user\\Desktop\\多變量報告\\data\\wq_pca.csv")
 vars = (pca$sdev)^2 # 從pca中取出標準差(pca$sdev)後再平方，計算variance(特徵值)
 props = vars / sum(vars)#每個主成分的解釋比例 = 各個主成分的特徵值/總特徵值
-cumulative.props = cumsum(props) # 累加前n個主成分的解釋比例
-#取前4個主成份，可以解釋 70.8% 的變異
-cumulative.props[4]
 # 使用plot()函式 畫陡坡圖
 plot(pca, # 放pca
      type="line", # 用直線連結每個點
-     main="Scree Plot for GSP raw", col = "darkred") # 主標題
+     pch = 16,
+     main="陡坡圖", col = "darkred") # 主標題
 abline(h=1, col="orange")#BY 
+cumulative.props = cumsum(props) # 累加前n個主成分的解釋比例
+#取前4個主成份，可以解釋 70.8% 的變異
+cumulative.props[4]
+
 # 累積解釋比例圖
 plot(cumulative.props, pch = 16, col = "darkred")
 
-top2 = pca$x[, 1:2]
-top2.ev = pca$rotation[,1:2]
+top2 = pca$x[, 1:4]
+top2.ev = pca$rotation[,1:4]
 fpca = top2.ev[,1]
 spca = top2.ev[,2]
 tpca = top2.ev[,3]
 fopca = top2.ev[,4]
-dotchart(fpca[order(fpca, decreasing = FALSE)], col="red", main = "第一主成份", pch)
+dotchart(fpca[order(fpca, decreasing = FALSE)], col="red", main = "第一主成份")
 dotchart(spca[order(spca, decreasing = FALSE)], col="red", main = "第二主成份")
-dotchart(fpca[order(tpca, decreasing = FALSE)], col="red", main = "第三主成份")
-dotchart(spca[order(fopca, decreasing = FALSE)], col="red", main = "第四主成份")
+dotchart(tpca[order(tpca, decreasing = FALSE)], col="red", main = "第三主成份")
+dotchart(fopca[order(fopca, decreasing = FALSE)], col="red", main = "第四主成份")
